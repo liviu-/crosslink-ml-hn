3cf21d7021e92ee59729be864d0a414daaa72f87
liviu- <hi@liviu.me>
2016-08-20 18:45:49 +0100

    Initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The versioning process follows a scheme based on [Semantic Versioning](http://semver.org/), and the CHANGELOG.md format is based on [Keep a Changelog](http://keepachangelog.com/).
+
+## 0.0.1 - 2016-08-20
+
+Initial release
--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
+import os
 from setuptools import setup, find_packages
+
+package_name = 'crosslinking_bot'
+version_file = 'version.py'
+
+def get_version(package_name=package_name, filename=version_file):
+    """Retrieve version from package_name/filename
+
+    Assumes that version is the only line in the file,
+    and it's a module-level declaration following 
+    the format of `__version__ = 'x.y.z'`
+    """
+    version_path = os.path.join(package_name, version_file)
+    return open(version_path).read().split('=')[-1].strip(' \'"\n')
 
 setup(
     name='hn_crosslinking_bot',
-    version='0.1dev',
+    url='https://github.com/liviu-/crosslink-ml-hn',
+    version=get_version(),
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -16,6 +31,5 @@ setup(
         'urltools'
     ],
     license='MIT',
-    url='https://github.com/liviu-/crosslink-ml-hn',
     long_description=open('README.md').read(),
 )
