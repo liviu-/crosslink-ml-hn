429689c4cda665310e2d7738aa10b63a08cf63dc
liviu- <hi@liviu.me>
2016-08-30 22:00:44 +0100

    Add `--version` flag (close #9)

--- crosslinking_bot/__init__.py
+++ crosslinking_bot/__init__.py
@@ -1,1 +1,12 @@
+import argparse
+
 from .version import __version__
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', '-v', action='version',
+                        version='%(prog)s {}'.format(__version__))
+
+    return parser.parse_args()
+
+parse_args()
